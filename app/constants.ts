0006b2814feb3594de09d75f7514a060df20933b
Alassiag <alassiag@gmail.com>
2022-01-21 20:44:49 +0100

    actualize guele store

--- app/constants.ts
+++ app/constants.ts
@@ -1,19 +1,20 @@
 export const INFORMATION = {
   avatar: "/assets/avatar.jpg",
   banner: "/assets/banner.jpg",
-  title: "URBN Catering",
-  description: "Galardonada pizza estilo New Haven en Palermo SOHO.",
-  phone: `5491141414141`,
-  sheet: `https://docs.google.com/spreadsheets/d/e/2PACX-1vQG1pOSbGxaJYzlOWF2t7KFyP5wtWo_EDS0crTllQtALvDDZBq7OwxvTDJwM4dmasW7wmNh6KFB443j/pub?output=csv`,
+  title: "TyniHome Deco",
+  description: "Accesorios y Deco para el hogar.",
+  phone: `5493472503340`,
+  sheet: `https://docs.google.com/spreadsheets/d/e/2PACX-1vRpdHYE5KHxCoEdt0dflEjrpHv6rOD46dcgE5roPqR22_uPMmW3GNu5hPz7nF72gD9yUk73lnOiSdoA/pub?output=csv`,
+  //https://docs.google.com/spreadsheets/d/e/2PACX-1vQG1pOSbGxaJYzlOWF2t7KFyP5wtWo_EDS0crTllQtALvDDZBq7OwxvTDJwM4dmasW7wmNh6KFB443j/pub?output=csv
   color: `teal`,
   social: [
     {
       name: "instagram",
-      url: `https://instagram.com/goncy.js`,
+      url: `https://instagram.com/tyni_home_deco`,
     },
     {
       name: "whatsapp",
-      url: `https://wa.me/5491141414141`,
+      url: `https://wa.me/5493472503340`,
     },
   ],
 };
